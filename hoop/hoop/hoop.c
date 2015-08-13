19f50702226d20bafac34f7a9c83b6df4146ec59
nerth <admin@nerth.net>
2015-08-12 20:37:25 -0400

    atmel studio project

---
+++ hoop/hoop/hoop.c
@@ -0,0 +1,17 @@
+/*
+ * hoop.c
+ *
+ * Created: 8/12/2015 8:30:03 PM
+ *  Author: nerth
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
